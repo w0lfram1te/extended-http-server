9d46a5919f6d40aee6bcbaeb0882666ea8ae491e
w0lfram1te <74439854+w0lfram1te@users.noreply.github.com>
2021-07-23 09:21:37 +0800

    test

--- ehttpserver.py
+++ ehttpserver.py
@@ -2,12 +2,51 @@
 
 import http.server
 import socketserver
+import os
 
 PORT = 8000
 
-if __name__ == '__main__':
-	Handler = http.server.SimpleHTTPRequestHandler
+class ExtendedHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, directory=None, **kwargs):
+        super().__init__(*args, **kwargs)
 
-	with socketserver.TCPServer(("", PORT), Handler) as httpd:
-		print("[*] Running python http server on port", PORT)
-		httpd.serve_forever()
+    def do_PUT(self):
+        """Serve a PUT request."""
+        print("[*] receiving PUT request")
+        # filename validation - check if file exists if yes increment
+
+        path = self.translate_path(self.path)
+        f = None
+
+        print(path)     
+        # if not os.path.exists:
+            # # support for file name modification will come later
+            # print("[-] file path already exists")
+            # self.send_error(500)
+            # return
+
+        try:
+            f = open(path, 'wb')
+
+        except OSError:
+            print("[-] file opening died")
+            self.send_error(500)
+            return
+
+        try:
+            self.copyfile(self.rfile, f)
+            print("done copying")
+            self.send_response(HTTPStatus.OK)
+            self.end_headers()
+            f.close()
+            return
+        except:
+            f.close()
+            return
+
+if __name__ == '__main__':
+    Handler = ExtendedHTTPRequestHandler
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("[*] Running python http server on port", PORT)
+        httpd.serve_forever()
